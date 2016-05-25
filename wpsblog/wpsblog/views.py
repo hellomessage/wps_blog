c95901f4beded612ad7723bec72799691259835a
message0316 <message0316@gmail.com>
2016-05-25 12:58:19 +0900

    update room view: crawl room data from zigbang api

--- wpsblog/wpsblog/views.py
+++ wpsblog/wpsblog/views.py
@@ -1,9 +1,13 @@
+import requests
+
 from django.http.response import HttpResponse
 
 #MVC Controller
 def home(request):
 	return HttpResponse("hello world han's")
 
-def room(requset, room_id):
-	print(https://api.zigbang.com/v1/items?detail=true&item_ids=4701501)
-	return HttpResponse("This is a room detail" + room_id)
+def room(request, room_id):
+	#방 번호 ( room_id ) 직방의 데이터를 그대로 보여주는 뷰(컨트롤러)
+	url = "https://api.zigbang.com/v1/items?detail=true&item_ids=" + room_id
+	response = requests.get(url)
+	return HttpResponse(response.content)
