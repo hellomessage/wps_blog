16d67586ba896f06266ff514656f167dcfa7419f
message0316 <message0316@gmail.com>
2016-05-25 12:39:51 +0900

    update requirments/production.txt: add requests for HTTP request

--- wpsblog/wpsblog/views.py
+++ wpsblog/wpsblog/views.py
@@ -5,4 +5,5 @@ def home(request):
 	return HttpResponse("hello world han's")
 
 def room(requset, room_id):
+	print(https://api.zigbang.com/v1/items?detail=true&item_ids=4701501)
 	return HttpResponse("This is a room detail" + room_id)
