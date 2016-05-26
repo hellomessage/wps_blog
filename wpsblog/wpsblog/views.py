126744f44568329beb5d96ed4ff199b49c62e254
message0316 <message0316@gmail.com>
2016-05-26 13:42:59 +0900

    refactor news view :seperate html code from python script

--- wpsblog/wpsblog/views.py
+++ wpsblog/wpsblog/views.py
@@ -2,6 +2,7 @@ import json
 import requests
 
 from django.http.response import HttpResponse
+from django.conf import settings
 
 def home(request):
 	return HttpResponse("<h1>hello world</h1><p>This is home page.</p>")
@@ -16,20 +17,11 @@ def room(request, room_id):
 	)
 
 def news(request):
-#	from IPython import embed; embed()
 	search = request.GET.get("search")	#html에서 get으로 넘기는 것을 받아옴 
-	print(search)
 
 	response = requests.get("https://watcha.net/home/news.json?page=1&per=50")
 	news_dict = json.loads(response.text)	# response.json()
 	news_list = news_dict.get("news")
-
-	form_html = """
-		<form method="GET" action="/news/">
-			<input type="text" name="search">
-			<input type="submit" value="검색">
-		</form>
-	"""
 
 	if search:
 		news_list = list(filter(
@@ -37,10 +29,9 @@ def news(request):
 			news_list,
 		))
 
-	content = "<h1>News</h1>" +\
-		"<p> This is news page.</p>" +\
-		form_html +\
-		"<p>{count} 개의 영화 뉴스 정보가 있습니다.</p>".format(count=len(news_list)) +\
+	with open(settings.BASE_DIR + "/wpsblog/templates/news.html", "r") as template:
+		content = template.read()
+		content +="<p>{count} 개의 영화 뉴스 정보가 있습니다.</p>".format(count=len(news_list)) +\
 		"".join([
 			"<h2>{title}</h2><img src={image_src}><p>{content}</p>".format(
 				title=news.get('title'),
@@ -49,9 +40,9 @@ def news(request):
 			)
 			for news
 			in news_list
-		]) 
-
-	return HttpResponse(
-		content
-	)
+		])
+ 
+		return HttpResponse(
+			content
+		)
 
