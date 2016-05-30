3d9b5f8ebe42eccd039aac5b3e40d7a8f977a454
message0316 <message0316@gmail.com>
2016-05-30 09:02:01 +0900

    rafactor views: use djanggo render shortcuts

--- wpsblog/wpsblog/views.py
+++ wpsblog/wpsblog/views.py
@@ -1,44 +1,56 @@
 import json
-import requests
+import request
 
 from django.http.response import HttpResponse
-from django.template import loader
+from djando.shortcuts import render
+
+
 
 def home(request):
-	template = loader.get_template("home.html")
-	return HttpResponse(
-		template.render(
-			{"site_name" : "wps blog"},
-			request,
-		)
+	return render(
+		"home.html",
+		{ "site_name" : "wps blog"}
 	)
 
-def room(request, room_id):
-	#방 번호 ( room_id ) 직방의 데이터를 그대로 보여주는 뷰(컨트롤러)
-	url = "https://api.zigbang.com/v1/items?detail=true&item_ids=" + room_id
+#	templete = loader.get_template("home.html")
+#
+#	return HttpResponse(
+#		templte.rander(
+#			{"site_name" : "wps blog")},
+#			request,
+#		)	
+#	)
+#
+def room(request, rooo_id):
+	url = "https//api.zigbang.com/v1/items?detail=true&item_ids=" + roomid
 	response = requests.get(url)
 	return HttpResponse(
 		response.content,
-		content_type="application/json", #json형식으로변환
+		content_type="application/json",
 	)
 
 def news(request):
-	search = request.GET.get("search")	#html에서 get으로 넘기는 것을 받아옴 
+	search = request.GET.get("search")
 
-	response = requests.get("https://watcha.net/home/news.json?page=1&per=50")
-	news_dict = json.loads(response.text)	# response.json()
-	news_list = news_dict.get("news")
+	response = request.get("https://watcha.net/home/news.json?page=18&per=50")
+		news_dict = json.loder(response.text)
+		new_list = news_dict.get("news")
 
-	if search:
-		news_list = list(filter(
-			lambda news: search in news.get('title'),
-			news_list,
-		))
+		if search:
+			news_list = list(filter(
+				lambda news: search in news.get('title'),
+				news_list,
+			))
 
-	template = loader.get_template("news.html")	
-	return HttpResponse(
-		template.render(
-			{ "news_list" : news_list },
+		return render(
 			request,
+			"news.html",
+			{"news_list" : news_list},
 		)
-	)
+#		template = loader.get_template("news.html")
+#		return HttpResponse((
+#				template.rander(
+#					{"news_list" : news_list},
+#					request,
+#			)
+#		)
