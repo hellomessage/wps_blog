8acd1ead977defc5da2d28051c4248d89a7642cc
message0316 <message0316@gmail.com>
2016-06-02 08:35:07 +0900

    add django-extensions

--- wpsblog/wpsblog/urls/policy.py
+++ wpsblog/wpsblog/urls/policy.py
@@ -1,10 +1,10 @@
-from django.conf.urls import url 
+from django.conf.urls import url
 
 from wpsblog.views import *
 
 
 urlpatterns = [
-    url(r'terms/$', terms, name="terms"), 
+    url(r'terms/$', terms, name="terms"),
     url(r'privacy/$', privacy, name="privacy"),
     url(r'disclaimer/$', disclaimer, name="disclaimer"),
 ]
