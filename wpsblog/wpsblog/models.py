cb64e93b96b4c0b4163b955f94653e144e58cb7e
message0316 <message0316@gmail.com>
2016-06-03 09:42:19 +0900

    update .gitignore settings

--- wpsblog/wpsblog/models.py
+++ wpsblog/wpsblog/models.py
@@ -3,9 +3,9 @@ from django.db import models
 
 class Post(models.Model):
     title = models.CharField(
-		max_length=120,    
+            max_length=120,
     )
     content = models.TextField()
-    
+
     def __str__(self):
         return self.title
