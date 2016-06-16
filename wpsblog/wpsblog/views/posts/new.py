d7fe34e341afebef69d484d598be481b87010450
message0316 <message0316@gmail.com>
2016-06-16 14:34:31 +0900

    update mypage, posts-create view: login_required

--- wpsblog/wpsblog/views/posts/new.py
+++ wpsblog/wpsblog/views/posts/new.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required 
 
 from wpsblog.models import Post
 
 
+@login_required
 def new(request):
     return render(
         request,
         "posts/new.html",
-        {}
+        {},
     )
