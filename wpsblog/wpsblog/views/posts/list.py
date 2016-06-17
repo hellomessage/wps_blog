2ea281d955e432bc8c3502d5dc24a9dc99ac04ab
message0316 <message0316@gmail.com>
2016-06-17 13:21:06 +0900

    update posts list view: add pagination

--- wpsblog/wpsblog/views/posts/list.py
+++ wpsblog/wpsblog/views/posts/list.py
@@ -7,12 +7,18 @@ from wpsblog.models import Post
 
 
 def list(request):
+    page = request.GET.get("page", 1)
+    per = request.GET.get("per", 5)
+
+    paginator = Paginator(Post.objects.public(), per)
+    posts = paginator.page(page)
 
     return render(
         request,
         "posts/list.html",
         {
-            "posts": Post.objects.public(),
+            #"posts": Post.objects.public(),
+            "posts": posts,
         },
     )
 
