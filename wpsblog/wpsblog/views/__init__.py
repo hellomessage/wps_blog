68be7984f313264466af1c0d23efe72fc130c0ba
message0316 <message0316@gmail.com>
2016-06-20 21:37:22 +0900

    update PostListView

--- wpsblog/wpsblog/views/__init__.py
+++ wpsblog/wpsblog/views/__init__.py
@@ -3,7 +3,7 @@ from .about import about
 from .room import room
 from .news import news
 from .policy import terms, privacy, disclaimer
-from .posts import list, detail, new, create, edit, update, delete,\
+from .posts import PostListView, detail, new, create, edit, update, delete,\
         comments_create, comments_edit, comments_update, comments_delete
 from .naver_posts import naver_posts_list
 from .auth import login, signup, logout, mypage
