9bc5b9c9da8f02c17edf9659d2fe04c2f40c1700
message0316 <message0316@gmail.com>
2016-06-21 22:04:59 +0900

    refactor posts create view: to PostCreateView

--- wpsblog/wpsblog/views/auth/logout.py
+++ wpsblog/wpsblog/views/auth/logout.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.core.urlresolvers import reverse
 from django.contrib.auth import logout
 from django.shortcuts import redirect
@@ -7,4 +8,9 @@ from django.views.generic import View
 class LogoutView(View):
     def get(self, request, *args, **kwargs):
         logout(request)
+        messages.add_message(
+            request,
+            messages.SUCCESS,
+            "성공적으로 로그아웃 되었습니다.",
+        )
         return redirect(reverse("home"))
--- wpsblog/wpsblog/views/posts/create.py
+++ wpsblog/wpsblog/views/posts/create.py
@@ -1,18 +1,19 @@
+from django.views.generic import View
 from django.shortcuts import redirect
 
 from wpsblog.models import Post
 
 
-def create(request):
-    title = request.POST.get("title")
-    content = request.POST.get("content")
-    image = request.FILES.get("image")
+class PostCreateView(View):
+    def post(self, request, *args, **kwargs):
+        title = request.POST.get("title")
+        content = request.POST.get("content")
+        image = request.FILES.get("image")
 
-    post = Post.objects.create(
-        user=request.user,
-        title=title,
-        content=content,
-        image=image,
-    )
-
-    return redirect(post)
+        post = Post.objects.create(
+            user=request.user,
+            title=title,
+            content=content,
+            image=image,
+        )
+        return redirect(post)
