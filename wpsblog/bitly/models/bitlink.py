8402a8b9d5fc39f524621ef0fbb5c280adb24695
message0316 <message0316@gmail.com>
2016-06-22 12:46:33 +0900

    init Bitlink model

--- wpsblog/bitly/models/bitlink.py
+++ wpsblog/bitly/models/bitlink.py
@@ -1,9 +1,8 @@
 from django.contrib.auth.models import User
 from django.db import models
-from django.core.urlresolvers import reverse
 
 
-class Bitlink(modles.Model):
+class Bitlink(models.Model):
 
     user = models.ForeignKey(User)
 
@@ -13,25 +12,9 @@ class Bitlink(modles.Model):
         blank=True,
         null=True,
     )
-
-    create_at = models.DateTimeField(auto_now_add=True)
-    update_at = models.DateTimeField(auto_now=True)
+    # timestamp
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.original_url
-
-    def get_absolute_url(self):
-        return reverse(
-            "bitly:redirect",
-            kqaegse={
-                "shorten_hash": self.shortt
-            }
-        )
-'''
-@reciver(post_save, sender=Bitlink)
-def post_save_bitlink(sender, instatnce, created, **kwargs):
-    if created:
-        hashids = Hashids(salt="awesome botlink", min_length=4)
-        instance.shorten_hash = hashids.encode(instnce.id)
-        instance.save()
-'''
