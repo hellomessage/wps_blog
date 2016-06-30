16a2f212a98c60448c653b1efaceef6dbc5e4f99
message0316 <message0316@gmail.com>
2016-06-30 10:49:16 +0900

    init post_save_user signal: create user_profile on post save user

--- wpsblog/users/models/user_profile.py
+++ wpsblog/users/models/user_profile.py
@@ -1,13 +1,32 @@
 from django.contrib.auth.models import User
 from django.db import models
-from django.db.modles.signals import Post_save
-from django.dispatch import receiver
+# model이 create될 때
+from django.db.models.signals import post_save
+# from django.dispatch import receiver
 
 
-Class UserProfile(models.Model):
-    @receiver(post_save, sender=User)
-    def post_save_user(sender, instance, created, **kwargs):
-        if created:
-            user_profile = UserProfile.objects.create(
-                user=instance,
-            )
+class UserProfile(models.Model):
+
+    user = models.OneToOneField(User)
+
+    phonenumber = models.CharField(
+        max_length=64,
+        blank=True,
+        null=True,
+    )
+
+    address = models.CharField(
+        max_length=64,
+        blank=True,
+        null=True,
+    )
+
+
+def post_save_user(sender, instance, created, **kwargs):
+
+    if created:
+        user_profile = UserProfile.objects.create(
+            user=instance,
+        )
+
+post_save.connect(post_save_user, sender=User)
